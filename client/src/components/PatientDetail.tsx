f4c23547700e6f86c6523780c7c549aa6149bcc0
Alex Uldahl Pedersen <alex@uldahl.dk>
2024-08-20 14:03:20 +0200

    patient name

--- client/src/components/PatientDetail.tsx
+++ client/src/components/PatientDetail.tsx
@@ -3,7 +3,7 @@ import {DiseasesAtom} from "../atoms/DiseasesAtom.tsx";
 import {DiagnosesAtom} from "../atoms/DiagnosesAtom.tsx";
 import {useAtom} from "jotai";
 import {PatientsAtom} from "../atoms/PatientsAtom.tsx";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {RouteParams} from "../models/RouteParams.ts";
 import {Diagnoses, Patients} from "../Api.ts";
 import {DiagnoseWithDisease, PatientWithDetails} from "../models/CompoundModels.tsx";
@@ -15,6 +15,8 @@ export default function PatientDetail() {
     const [patients, setPatients] = useAtom(PatientsAtom);
     const [diagnoses, setDiagnoses] = useAtom(DiagnosesAtom);
     const [diseases, setDiseases] = useAtom(DiseasesAtom);
+    const [isEdit, setIsEdit] = useState(false);
+    const [newName, setNewName] = useState('');
     const params = useParams();
     const navigate = useNavigate();
     const [currentPatient, setCurrentPatient] = useState<Patients>()
@@ -32,33 +34,66 @@ export default function PatientDetail() {
 
     return (
         <div>
-            <h1 className="menu-title text-5xl m-5">Patient name: {currentPatient.name}</h1>
-            {
-                diagnoses.filter(d => d.patient_id == currentPatient?.id).map(d => {
-                    const disease = diseases.find(disease => disease.id == d.disease_id);
-                    return {...d, disease: disease!};
-                }).map(d => {
-                    return <div key={d?.id} className="card">
-                        <div className="card-body bg-base-200">
 
-                            <h2 className="card-title">Diagnose with: {d?.disease.name}</h2>
-                            <h2 className="card-title">The {new Date(d?.diagnosis_date!).toLocaleDateString()}</h2>
-                        </div>
-                        <button className="btn btn-error" onClick={() => {
-                            apiClient.patients.patientsDelete({id: 'eq.'+currentPatient?.id}).then(() => {
-                                setPatients(patients.filter(p => p.id != currentPatient.id));
-                                setDiagnoses(diagnoses.filter(d => d.patient_id != currentPatient.id));
-                                setDiseases(diseases.filter(d => d.id != currentPatient.id));
-                                navigate('/patients');
-                                toast.success("Patient " + currentPatient?.name + " has been deleted");
-                            });
-                        }}>Delete patient
-                        </button>
-                    </div>
+            <h3 className="card-title text-3xl m-5">Diseases for {currentPatient.name}:</h3>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                    <tr>
+                        <th></th>
+                        <th>Disease</th>
+                        <th>Diagnosis Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {diagnoses
+                        .filter(d => d.patient_id == currentPatient?.id)
+                        .map((d, index) => {
+                            const disease = diseases.find(disease => disease.id == d.disease_id);
+                            return (
+                                <tr key={d.id} className={index % 2 === 0 ? "bg-base-200" : ""}>
+                                    <th>{index + 1}</th>
+                                    <td>{disease ? disease.name : 'Unknown'}</td>
+                                    <td>{new Date(d.diagnosis_date!).toLocaleDateString()}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                    </tbody>
+                </table>
+                <h1 className="menu-title text-xl">Patient name: &nbsp;
+                    {
+                        isEdit ?
+                            <><input  placeholder={currentPatient.name} className="input-md"
+                                          value={newName}
+                                          onChange={e => setNewName(e.target.value)}  />
+                            <button className="btn btn-primary" onClick={() => {
+                                apiClient.patients.patientsPartialUpdate({ name: newName,}, {id: 'eq.' + currentPatient.id}).then(() => {
+                                    setPatients(patients.map(p => p.id == currentPatient.id ? {...p, name: newName} : p));
+                                    setIsEdit(false);
+                                    toast.success("Patient name updated to " + newName);
+                                });
+                            }}>Update!</button>
+                        </> : <button onClick={() => setIsEdit(true)} className="btn text-xl">{currentPatient.name} ✍️</button>
+                    }
 
-                })
-            }
 
+                </h1>
+            </div>
+
+
+            <div className="fixed bottom-4 right-4 z-50">
+                <button className="btn btn-error" onClick={() => {
+                    apiClient.patients.patientsDelete({id: 'eq.' + currentPatient?.id}).then(() => {
+                        setPatients(patients.filter(p => p.id != currentPatient.id));
+                        setDiagnoses(diagnoses.filter(d => d.patient_id != currentPatient.id));
+                        setDiseases(diseases.filter(d => d.id != currentPatient.id));
+                        navigate('/patients');
+                        toast.success("Patient " + currentPatient?.name + " has been deleted");
+                    });
+                }}>Delete patient "{currentPatient.name}"
+                </button>
+            </div>
 
 
         </div>
--- client/src/components/PatientsList.tsx
+++ client/src/components/PatientsList.tsx
@@ -10,38 +10,40 @@ export default function PatientsList() {
     const [modalController, setModalController] = useAtom(EnrollPatientModalAtom);
     const navigate = useNavigate();
 
-    return(<div className="flex flex-col justify-between">
-        <div>
-            {patients.map((patient) => {
-                return <div key={patient.id} className="card bg-base-100 w-1/2 shadow-xl m-10">
-                    <div className="card-body"><h2 className="card-title">{patient.name}</h2>
+    return (
+        <div className="flex flex-col justify-between">
+            <div>
+                {patients.map((patient) => {
+                    return <div key={patient.id} className="card bg-base-100 w-1/2 shadow-xl m-10">
+                        <div className="card-body"><h2 className="card-title">{patient.name}</h2>
 
-                        <div className="card-actions flex justify-end">
+                            <div className="card-actions flex justify-end">
 
-                            <button onClick={() => navigate('/patients/' + patient.id)}
-                                    className="btn btn-secondary">See details for {patient.name}
-                            </button>
+                                <button onClick={() => navigate('/patients/' + patient.id)}
+                                        className="btn btn-secondary">See details for {patient.name}
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            })}
-        </div>
-        <div className="fixed bottom-4 right-4 z-50">
-            <button
-                onClick={() => {
-                    setModalController({modalOpen: true});
-                    console.log(modalController.modalOpen);
-                }}
-                className="btn btn-circle btn-primary shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4"/>
-                </svg>
-            </button>
+                })}
+            </div>
+            <div className="fixed bottom-4 right-4 z-50">
+                <button
+                    onClick={() => {
+                        setModalController({modalOpen: true});
+                        console.log(modalController.modalOpen);
+                    }}
+                    className="btn btn-circle btn-primary shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4"/>
+                    </svg>
+                </button>
+            </div>
             <EnrollPatientModal/>
-        </div>
 
-    </div>)
+        </div>
+    )
 }
